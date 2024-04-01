96e695bcc66515c3cc6ba6fd73d4a9cb06d823f6
Esteban Poveda <hepoveda.g@gmail.com>
2024-04-01 07:48:14 -0500

    Culminacion de tutorial

--- myApp/forms.py
+++ myApp/forms.py
@@ -3,7 +3,7 @@ from myApp.models import Project
 
 class createNewTask(forms.Form):
     title = forms.CharField(label="Titulo de la tarea", max_length=200, widget = forms.TextInput(attrs = {'class' : 'input'}))
-    description = forms.CharField(label = "Descripcion de la tarea", widget=forms.Textarea)
+    description = forms.CharField(label = "Descripcion de la tarea", widget=forms.Textarea(attrs = {'class' : 'input'}))
     project_id = forms.ChoiceField(choices = Project.objects.values_list('id', 'name')) 
 
     def __init__(self, *args, **kwargs):
@@ -11,4 +11,4 @@ class createNewTask(forms.Form):
         self.fields['project_id'].choices = Project.objects.values_list('id', 'name')
 
 class createNewProject(forms.Form):
-    name = forms.CharField(label="Nombre del nuevo proyecto", max_length = 200) 
+    name = forms.CharField(label="Nombre del nuevo proyecto", max_length = 200, widget = forms.TextInput(attrs={'class' : 'input'})) 
